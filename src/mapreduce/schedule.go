b5ea7551073a73c17bcb732d98aa50902d9da83f
wujinggrt <wujinggrt@qq.com>
2019-04-14 14:07:05 +0800

    lab1 part 4

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -39,24 +39,27 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 		wg.Add(1)
 		go func (taskNum int) {
 			defer wg.Done()
-			worker := <- registerChan 
-			taskArgs := DoTaskArgs {
-				jobName,
-				mapFiles[taskNum],
-				phase,
-				taskNum,
-				n_other,
-			}
-			// 加上这句也没啥，可以看出，reduce 阶段文件名是没用的
-			// if phase == reducePhase {
-			// 	taskArgs.File = ""
-			// }
-			ok := call(worker, "Worker.DoTask", &taskArgs, nil)
-			if ok {
-				// 这儿不用 goroutine 会让 reduces phase 丢失信息
-				go func () {
-					registerChan <- worker
-				}()
+			for {
+				worker := <- registerChan 
+				taskArgs := DoTaskArgs {
+					jobName,
+					mapFiles[taskNum],
+					phase,
+					taskNum,
+					n_other,
+				}
+				// 加上这句也没啥，可以看出，reduce 阶段文件名是没用的
+				// if phase == reducePhase {
+				// 	taskArgs.File = ""
+				// }
+				ok := call(worker, "Worker.DoTask", &taskArgs, nil)
+				if ok {
+					// 这儿不用 goroutine 会让 reduces phase 丢失信息
+					go func () {
+						registerChan <- worker
+					}()
+					break
+				}
 			}
 		}(i)
 	}
